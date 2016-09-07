a2c7f6a4d9584fa461b375ab7a4ca41227b4c33a
brighty1982 <abright@utilitywarehouse.co.uk>
2016-09-07 20:05:23 +0100

    lesson 2 initial commit

--- src/main/groovy/Lesson1.groovy
+++ src/main/groovy/Lesson1.groovy
@@ -1,6 +1,24 @@
 class Lesson1 {
 
+    static void GetInputString(){
+
+        def inputstring = ''
+
+        while(inputstring == ''){
+            println('What is the input string?')
+            inputstring = System.in.newReader().readLine()
+            if(inputstring == ''){
+                println('You must enter a value.')
+            } else {
+                println(inputstring + ' has ' + inputstring.length() + ' characters')
+            }
+        }
+
+
+    }
+
+
     static void main(String[] args){
-        println('Hello World')
+        GetInputString()
     }
 }
